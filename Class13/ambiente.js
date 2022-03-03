3a72e91c99ce741e6540fb5e3228b59031e1e5a8
Marcos Melo <marcaos.melo@gmail.com>
2022-03-03 11:11:00 +0000

    do while

--- Class13/ambiente.js
+++ Class13/ambiente.js
@@ -1,5 +1,15 @@
+// Laço de Repetição com do-while
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 6)
+
+// Laço de repetição simples com o While
+/*
 var c = 1
 while ( c <= 6) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
