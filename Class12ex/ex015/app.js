331bb8726795357af075bfc61c54ee814f729364
Marcos Melo <marcaos.melo@gmail.com>
2022-03-09 15:27:26 +0000

    Exercicio 15 verificador de idade que apresenta uma foto conforme a idade

---
+++ Class12ex/ex015/app.js
@@ -0,0 +1,12 @@
+function verificar() {
+    //window.alert('Funcionou')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] - Verifique os dados e tente novamente!')
+    } else {
+        //window.alert('Tudo OK')
+    }
+}
