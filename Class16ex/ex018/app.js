facd665204201fbb0c23613a3832b46a16e0190a
Marcos Melo <marcaos.melo@gmail.com>
2022-03-20 18:24:38 +0000

    Termino do ex18  - dom, lista, funcoes

--- Class16ex/ex018/app.js
+++ Class16ex/ex018/app.js
@@ -12,7 +12,7 @@ function isNumber(n) {
     }
 }
 
-function inLista (n, l) {
+function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -22,23 +22,40 @@ function inLista (n, l) {
 
 function adicionar() {
     if (isNumber(num.value) && !inLista(num.value, valores)) {
-        window.alert('Tudo OK')
-    } else {
-        window.alert('Valor Inválido ou já se encontra na lista') 
-    }
-
-    /*
-    if (num.value.length == 0) {
-        window.alert('Por Favor, Digite um Número')
-    } else if ( num.value <= 0 || num.value > 100) {
-        window.alert('Por Favor, Digite um Número entre 1 e 100')        
-    } else {
-        let n = Number(num.value)
+        valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${n} adicionado`
-        item.value = `txtin${n}`
-        txtin.appendChild(item)
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor Inválido ou Já se Encontra na Lista')
     }
-    */
+    num.value = ''
+    num.focus() 
+}
 
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]            
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>No Total temos ${tot} números inseridos.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A minha média dos valores digitados é ${media}.</p>`
+    }
 }
